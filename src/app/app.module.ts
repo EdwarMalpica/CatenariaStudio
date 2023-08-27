478b53affd74a3535b6cbf32380426d2053a45a1
EdwarMalpica <edwar.malpica@uptc.edu.co>
2023-08-26 21:14:12 -0500

    Dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SectionContentLeftComponent } from './components/section-content-left/s
 import { ServicesTitleComponent } from './components/home/services-title/services-title.component';
 import { MoreServicesComponent } from './components/home/more-services/more-services.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ContactoComponent } from './components/contacto/contacto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [EngineService],
   bootstrap: [AppComponent]
