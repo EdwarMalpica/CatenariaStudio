a8d055945a87935fabd663d8bc624dfeffbae0ca
Vivi <vivi_c11@yahoo.es>
2023-09-20 22:30:54 -0500

    componente términos y condiciones
    vista componente términos y condiciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TerminosComponent } from './terminos/terminos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'terminos', component: TerminosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
