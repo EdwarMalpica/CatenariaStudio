d84bafbfd909173793b89111f827047fb55f4335
YANM <yamid.nino02@uptc.edu.co>
2023-09-21 22:14:00 -0500

    Se creo el authService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { Credential } from '../models/auth/credential';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +15,26 @@ export class LoginComponent {
   password: string = '';
 
   constructor(private activatedRoute: ActivatedRoute,
-    private router: Router){
+    private router: Router,
+    private authService: AuthService) {
 
   }
 
   login() {
     // Lógica de autenticación
-    // se verifican las credenciales de la API
+    // se verifican las credenciales de la API  
     if (this.username === 'usuario' && this.password === 'contraseña') {
-      alert('Inicio de sesión exitoso');
+      this.authService.login(new Credential(this.username, this.password)).subscribe((dataResponse: any) => {
+        console.log(dataResponse);
+        alert('Inicio de sesión exitoso')
+      },
+        (error: any) => {
+          console.log(error);
+          alert('Credenciales incorrectas');
+        }
+      );
     } else {
-      alert('Credenciales incorrectas');
+      alert('Complete los campos');
     }
   }
 
