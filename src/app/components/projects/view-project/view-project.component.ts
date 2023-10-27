356a56c6b87a50f42549fda659aeb653af12e669
EdwarMalpica <edwar.malpica@uptc.edu.co>
2023-10-27 14:57:04 -0500

    Ver modelos de forma estatica

--- src/app/components/projects/view-project/view-project.component.ts
+++ src/app/components/projects/view-project/view-project.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiServiceService } from 'src/app/services/api/api-service.service';
+import { EngineService } from 'src/app/services/engine.service';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 
 @Component({
   selector: 'app-view-project',
   templateUrl: './view-project.component.html',
   styleUrls: ['./view-project.component.css'],
 })
-export class ViewProjectComponent {
+export class ViewProjectComponent implements OnInit {
   /** Este arreglo es solo a modo de ejemplo para tener algo que cargar
   en la sección de comentarios y que se muestre disqus*/
   proyecto: any;
   img: any[] = [];
   id: string;
-  constructor(private api: ApiServiceService, private route: ActivatedRoute) {
+  modelPath = '';
+  @ViewChild('canvasModel') canvasModel: ElementRef<HTMLCanvasElement>;
+
+  constructor(
+    private api: ApiServiceService,
+    private route: ActivatedRoute,
+    private ngse: EngineService
+  ) {
     this.route.params.subscribe((params) => {
       this.id = params['id'];
       this.posts[0].id = params['id'];
@@ -29,8 +41,37 @@ export class ViewProjectComponent {
       this.img = this.proyecto.files.filter((file: any) =>
         this.noContieneGLBorGLTF(file.formato)
       );
-      console.log(this.img);
+      this.modelPath = this.proyecto.files.filter((file: any) =>
+        this.contieneGLBorGLTF(file.formato)
+      )[0].path;
     });
+  }
+
+  setScene() {
+    this.ngse.createScene(this.canvasModel);
+
+  }
+  ngOnInit(): void {
+    if (document.readyState === 'complete') {
+      // Aquí puedes escribir el código que quieres que se ejecute después de que el DOM se haya cargado
+      this.ngse.createScene(this.canvasModel);
+      //this.engService.animate();
+    } else {
+      document.addEventListener('DOMContentLoaded', (event) => {
+        this.ngse.createScene(this.canvasModel);
+        //this.engService.animate();
+      });
+    }
+  }
+
+  animate() {}
+
+  cargarModelo() {
+    return new URL(this.apiUrl + this.modelPath, import.meta.url);
+  }
+
+  contieneGLBorGLTF(cadena: string): boolean {
+    return cadena.includes('glb') || cadena.includes('gltf');
   }
 
   noContieneGLBorGLTF(cadena: string): boolean {
